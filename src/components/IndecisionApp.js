ef60bfb5a0475d5c4641bed18c668f27ef44d376
Frederico Martini <fredmalmeida@gmail.com>
2019-08-23 19:39:07 -0300

    andamento 23/08/2019 utilizando variveis p/ estilizar scss

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -84,13 +84,15 @@ export default class IndecisionApp extends Component {
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
-        <Action hasOptions={options.length > 0} handlePickOne={this.handlePickOne} />
-        <Options
-          options={options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOne={this.handleDeleteOne}
-        />
-        <AddOption handleAddOne={this.handleAddOne} />
+        <div className="container">
+          <Action hasOptions={options.length > 0} handlePickOne={this.handlePickOne} />
+          <Options
+            options={options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOne={this.handleDeleteOne}
+          />
+          <AddOption handleAddOne={this.handleAddOne} />
+        </div>
         <OptionModal
           selectedOption={selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
